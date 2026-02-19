da9574dc9a071d77cf8845395cb6d83cf29512cb
Herson Cruz <herson@datolab.com>
2026-02-18 20:11:36 -0600

    fix(enumerator): filter for valid protocols to prevent AttributeError on hostnames list

--- modules/enumerator.py
+++ modules/enumerator.py
@@ -5,8 +5,19 @@ class ServiceEnumerator:
     def enumerate(self):
         services = {}
         print("Enumerating services...")
+        # Ensure scan data is valid
+        if not self.scan_data or 'scan' not in self.scan_data:
+            return services
+            
         for host in self.scan_data['scan']:
-            for protocol in self.scan_data['scan'][host].keys():
+            for protocol in self.scan_data['scan'][host]:
+                if protocol not in ['tcp', 'udp']:
+                    continue
+                
+                # Double check it is a dict before accessing keys
+                if not isinstance(self.scan_data['scan'][host][protocol], dict):
+                    continue
+
                 for port in self.scan_data['scan'][host][protocol].keys():
                     service = self.scan_data['scan'][host][protocol][port]['name']
                     version = self.scan_data['scan'][host][protocol][port]['version']
